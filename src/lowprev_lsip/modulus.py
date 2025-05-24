6c5c92683eb096fd2d479d1430df66deb1832d5f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2025-05-24 14:37:16 +0100

    Improved code for modulus of continuity (brute force implementation).

--- src/lowprev_lsip/modulus.py
+++ src/lowprev_lsip/modulus.py
@@ -15,58 +15,108 @@ class Domain:
     linear_constraint: LinearConstraint | None = None
 
 
-def get_points(n: int, bounds: Bounds) -> npt.NDArray:
+def _get_points(n: int, bounds: Bounds) -> Iterable[npt.NDArray]:
     points = np.linspace(bounds.lb[-1], bounds.ub[-1], n)
     if len(bounds.lb) == 1:
-        return points
+        return (np.array([x]) for x in points)
     else:
-        return np.array([
+        return (
             np.append(x, y)
-            for x in get_points(n, Bounds(bounds.lb[:-1], bounds.ub[:-1]))
+            for x in _get_points(n, Bounds(bounds.lb[:-1], bounds.ub[:-1]))
             for y in points
-            ]
         )
 
 
-def get_points_with_linear_constraint(
-    points: Iterable[npt.NDArray], linear_constraint: LinearConstraint
-) -> Iterable[npt.NDArray]:
-    for x in points:
-        if linear_constraint.residual(x) >= 0:
-            yield x
+def get_points(n: int, domain: Domain, tol=1e-6) -> Iterable[npt.NDArray]:
+    def is_feasible(x: npt.NDArray):
+        assert domain.linear_constraint is not None
+        sl: npt.NDArray
+        sb: npt.NDArray
+        sl, sb = domain.linear_constraint.residual(x)
+        return np.min([sl, sb]) >= -tol
+
+    points = _get_points(n, domain.bounds)
+    return (
+        points
+        if domain.linear_constraint is None
+        else (x for x in points if is_feasible(x))
+    )
 
 
-def get_neighbourhood_at(
-    metric: Callable[[T, T], float], points: Iterable[T], x: T, z: float
-) -> Iterable[T]:
-    return (y for y in points if metric(x, y) <= z)
+def get_neighbourhood_from_metric(
+    metric: Callable[[T, T], float],
+    points: Sequence[T],
+    z: float,
+    tol=1e-6,
+) -> Callable[[T], Iterable[T]]:
+    def _(x: T) -> Iterable[T]:
+        return (y for y in points if metric(x, y) <= z + tol)
+
+    return _
 
 
-def get_max_norm_neighbourhood_at(
-    n: int, x: npt.NDArray, z: float
-) -> Iterable[npt.NDArray]:
-    return get_points(n, Bounds(x - 0.5 * z, x + 0.5 * z))
+def get_neighbourhood_from_max_norm(
+    n: int,
+    domain: Domain,
+    z: float,
+    tol=1e-6,
+) -> Callable[[npt.NDArray], Iterable[npt.NDArray]]:
+    def _(x: npt.NDArray) -> Iterable[npt.NDArray]:
+        lb = np.max([domain.bounds.lb, x - z], axis=0)
+        ub = np.min([domain.bounds.ub, x + z], axis=0)
+        neighbourhood_domain = Domain(Bounds(lb, ub), domain.linear_constraint)
+        return get_points(n, neighbourhood_domain, tol=tol)
+
+    return _
 
 
 def modulus_of_continuity_at(
-    x: T,
     fun: Callable[[T], float],
-    neighbourhood: Sequence[T],
-) -> float:
-    return max(abs(fun(x) - fun(y)) for y in neighbourhood)
+    neighbourhood: Iterable[T],
+    x: T,
+) -> tuple[float, T, T]:
+    """Calculate maximum distance between *fun* of *x*
+    and *fun* of all values in *neighbourhood*.
+    """
+    return max(
+        ((abs(fun(x) - fun(y)), x, y) for y in neighbourhood), key=lambda x: x[0]
+    )
 
 
-def measure_modulus_of_continuity_1(
+def measure_modulus_of_continuity_from_neighbourhood(
+    fun: Callable[[T], float],
+    neighbourhood: Callable[[T], Iterable[T]],
+    points: Sequence[T],
+) -> tuple[float, T, T]:
+    """Calculate modulus of continuity from given points and neighbourhoods."""
+    return max(modulus_of_continuity_at(fun, neighbourhood(x), x) for x in points)
+
+
+def measure_modulus_of_continuity_from_metric(
+    fun: Callable[[T], float],
     metric: Callable[[T, T], float],
     points: Sequence[T],
-    fun: Callable[[T], float],
     z: float,
-) -> float:
+    tol=1e-6,
+) -> tuple[float, T, T]:
     """Calculate modulus of continuity using a domain.
     This function implements the definition directly.
     Very slow (N^2 evaluations if the domain has N points).
     """
-    return max(
-        modulus_of_continuity_at(x, fun, get_neighbourhood_at(metric, points, x, z))
-        for x in points
+    return measure_modulus_of_continuity_from_neighbourhood(
+        fun, get_neighbourhood_from_metric(metric, points, z, tol=tol), points
+    )
+
+
+def measure_modulus_of_continuity_from_max_norm(
+    fun: Callable[[npt.NDArray], float],
+    domain: Domain,
+    n_points: int,
+    n_neighbourhood: int,
+    z: float,
+    tol=1e-6,
+) -> tuple[float, npt.NDArray, npt.NDArray]:
+    points = list(get_points(n_points, domain, tol))
+    return measure_modulus_of_continuity_from_neighbourhood(
+        fun, get_neighbourhood_from_max_norm(n_neighbourhood, domain, z), points
     )
--- test/test_modulus.py
+++ test/test_modulus.py
@@ -1,11 +1,110 @@
+from collections.abc import Callable, Sequence
+
 import numpy as np
-import numpy.testing as npt
-from scipy.optimize import Bounds
+import numpy.typing as npt
+import pytest
+from scipy.optimize import Bounds, LinearConstraint
 
-from lowprev_lsip.modulus import Domain, get_points
+from lowprev_lsip.modulus import (
+    Domain,
+    get_neighbourhood_from_max_norm,
+    get_points,
+    measure_modulus_of_continuity_from_max_norm,
+    measure_modulus_of_continuity_from_metric,
+    measure_modulus_of_continuity_from_neighbourhood,
+)
 
 
-def test_points() -> None:
-    npt.assert_allclose(
-        get_points(3, Bounds([-1, 0], [2, 2])), np.array([[-1, 0], [-1, 1], [-1, 2]])
+def assert_points_equal(
+    actual: Sequence[npt.NDArray], expected: Sequence[Sequence[float]]
+) -> None:
+    assert len(actual) == len(expected)
+    for x, y in zip(actual, expected):
+        assert x == pytest.approx(y)
+
+
+def test_get_points_1() -> None:
+    assert_points_equal(
+        list(get_points(3, Domain(Bounds([-1, 0], [2, 2])))),
+        [
+            [-1, 0],
+            [-1, 1],
+            [-1, 2],
+            [0.5, 0],
+            [0.5, 1],
+            [0.5, 2],
+            [2, 0],
+            [2, 1],
+            [2, 2],
+        ],
     )
+
+
+def test_get_points_2() -> None:
+    domain = Domain(
+        # -1 <= x <= 2, 0 <= y <= 2
+        Bounds([-1, 0], [2, 2]),
+        # 0 <= x - y <= 2
+        LinearConstraint([[1, -1]], [0], [2]),
+    )
+    xs = list(get_points(3, domain))
+    assert_points_equal(
+        xs,
+        [
+            [0.5, 0],
+            [2, 0],
+            [2, 1],
+            [2, 2],
+        ],
+    )
+
+
+def test_get_neighbourhood_from_max_norm() -> None:
+    domain = Domain(
+        # -1 <= x <= 2, 0 <= y <= 2
+        Bounds([-1, 0], [2, 2]),
+        # 0 <= x - y <= 2
+        LinearConstraint([[1, -1]], [0], [2]),
+    )
+    xs = list(get_neighbourhood_from_max_norm(3, domain, 0.2)(np.array([0.1, 0.1])))
+    assert_points_equal(xs, [[0.1, 0.0], [0.3, 0.0], [0.3, 0.15], [0.3, 0.3]])
+
+
+@pytest.mark.parametrize(
+    "fun,z,expected",
+    [
+        (lambda x: x, 0.1, 0.1),
+        (lambda x: x**2, 0.2, 1 - 0.8**2),
+        (lambda x: -2 * x, 0.3, 0.6),
+    ],
+)
+def test_modulus_of_continuity_from_metric(
+    fun: Callable[[float], float], z: float, expected: float
+) -> None:
+    points = [x for x in np.linspace(0, 1, 100)]
+    mod, x0, x1 = measure_modulus_of_continuity_from_metric(
+        fun, lambda x, y: abs(x - y), points, z
+    )
+    assert mod == pytest.approx(expected, rel=0.1)  # large error due to poor grid
+    assert abs(fun(x0) - fun(x1)) == pytest.approx(mod)  # should be exact
+    assert np.max(np.abs(x0 - x1)) == pytest.approx(z, abs=0.01)  # 100 points, so 1/100
+
+
+@pytest.mark.parametrize(
+    "fun,z,expected",
+    [
+        (lambda x: x[0], 0.1, 0.1),
+        (lambda x: x[0] ** 2, 0.2, 1 - 0.8**2),
+        (lambda x: -2 * x[0], 0.3, 0.6),
+    ],
+)
+def test_modulus_of_continuity_from_max_norm(
+    fun: Callable[[npt.NDArray], float], z: float, expected: float
+) -> None:
+    domain = Domain(Bounds(0, 1))
+    mod, x0, x1 = measure_modulus_of_continuity_from_max_norm(
+        fun, domain, 100, 10, z
+    )
+    assert mod == pytest.approx(expected)
+    assert abs(fun(x0) - fun(x1)) == pytest.approx(mod)
+    assert np.max(np.abs(x0 - x1)) == pytest.approx(z)
